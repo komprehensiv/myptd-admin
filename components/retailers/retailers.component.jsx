d3cfc72d3787329fb58581fe3b8be1ccce58e23b
Jason Steele <jason.omnibus@gmail.com>
2020-12-12 11:29:18 -0500

    surcharges

--- components/retailers/retailers.component.jsx
+++ components/retailers/retailers.component.jsx
@@ -3,18 +3,18 @@ import { useStoreState, useStoreActions } from 'easy-peasy'
 import { Grid, TextField, Paper, MenuItem } from "@material-ui/core";
 import { NextButtonWrapper, PaperInner, TextFieldWrapper, NextButton } from "./retailers.styles";
 import { isFalsy } from '../../lib/is-falsy'
+import { isTruthy } from '../../lib/is-truthy'
+import find from 'lodash/find'
 
 const Retailers = () => {
-    const { retailers } = useStoreState(({ retailers }) => retailers);
-    const { fetchRetailers } = useStoreActions(({ retailers }) => retailers);
+    const { retailer, retailers } = useStoreState(({ retailers }) => retailers);
+    const { fetchRetailers, updateRetailer, updateSelectedRetailer } = useStoreActions(({ retailers }) => retailers);
     
     useEffect(() => {
         fetchRetailers();
     }, []);
 
     if (isFalsy(retailers)) return null;
-
-    console.log('RETAILERS', retailers)
 
     const getValue = field => isTruthy(field) ? field : '';
 
@@ -23,9 +23,9 @@ const Retailers = () => {
         return true;
     }
 
-    const updateSetting = () => {
-        debugger;
-        return true;
+    const setSelectedRetailer = ({value}) => {
+        const values = find(retailers, { 'id': parseInt(value) });
+        updateSelectedRetailer(values);
     }
 
     return (
@@ -33,36 +33,60 @@ const Retailers = () => {
         <Grid item xs={ 1 } />
         <Grid item xs={10}>
             <Paper>
-            <PaperInner>
-                <Grid container spacing={24}>
-                <Grid item xs={12} sm={4}>
-                    <TextFieldWrapper>
-                    <TextField 
-                        label="Retailer" 
-                        onChange={() => updateSetting({ key: 'retailer', value: event.srcElement.dataset.value })}
-                        fullWidth
-                        select>
-                        {/* value={getValue(retailer)}> */}
-                        {
-                            retailers.map(r => (
-                            <MenuItem key={r.id} value={r.id}>{r.name}</MenuItem>
-                            ))
-                        }
-                    </TextField>
-                    </TextFieldWrapper>
-                </Grid>
-                <Grid item xs={ 12 }>
-                    <NextButtonWrapper>
-                    <NextButton
-                        color="primary" 
-                        size="large"
-                        onClick={ () => submitForm() }>
-                        Next
-                    </NextButton>
-                    </NextButtonWrapper>
-                </Grid>
-                </Grid>
-            </PaperInner>
+                <PaperInner>
+                    <Grid 
+                        container 
+                        spacing={12} 
+                        alignItems="center"
+                        justify="center" 
+                        spacing={0}>
+                        <Grid item xs={12} sm={6}>
+                            <TextFieldWrapper>
+                            <TextField 
+                                label="Retailer" 
+                                onChange={() => setSelectedRetailer({ value: event.srcElement.dataset.value })}
+                                fullWidth
+                                select>
+                                {
+                                    retailers.map(r => (
+                                    <MenuItem key={r.id} value={r.id}>{r.name}</MenuItem>
+                                    ))
+                                }
+                            </TextField>
+                            </TextFieldWrapper>
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={12}>
+                        <Grid item xs={12} sm={4}>
+                            <TextFieldWrapper>
+                            <TextField
+                                label='Delivery Charge'
+                                placeholder=''
+                                onChange={() => updateRetailer({ key: 'deliverySurcharge', value: event.target.value })}
+                                fullWidth
+                                value={getValue(retailer.deliverySurcharge)}
+                            />
+                            </TextFieldWrapper>
+                        </Grid>
+                    </Grid>
+                    <Grid 
+                        container 
+                        spacing={12}
+                        alignItems="center"
+                        justify="center" 
+                        spacing={0}> 
+                        <Grid item xs={ 12 }>
+                            <NextButtonWrapper>
+                            <NextButton
+                                color="primary" 
+                                size="large"
+                                onClick={ () => submitForm() }>
+                                Next
+                            </NextButton>
+                            </NextButtonWrapper>
+                        </Grid>
+                    </Grid>
+                </PaperInner>
             </Paper>
         </Grid>
         <Grid item xs={ 1 } />
--- model/retailers.model.js
+++ model/retailers.model.js
@@ -1,11 +1,17 @@
 import { thunk, action } from 'easy-peasy';
 import getConfig from 'next/config';
+import find from 'lodash/find';
 
 const { publicRuntimeConfig: { RETAILERS_ENDPOINT }} = getConfig();
 
 const retailerModel = {
   retailers: [],
-  
+  retailer: {
+    deliverySurcharge: '',
+    deliveryPieceSurcharge: '',
+    longDistanceSurcharge: '',
+    longDistancePieceSurcharge: ''
+  },
   fetchRetailers: thunk(async (actions, payload) => {
     const response = await fetch(RETAILERS_ENDPOINT);
     const { data } = await response.json();
@@ -13,7 +19,15 @@ const retailerModel = {
   }),
   setRetailers: action((state, payload) => {
     state.retailers = payload;
-  })
+  }),
+  updateSelectedRetailer: action((state, payload) => {
+    debugger;
+    state.retailer = payload;
+  }),
+  updateRetailer: action((state, payload) => {
+    const { key, value } = payload;
+    state.retailer[key] = value;
+  }),
 }
 
 export default retailerModel;
